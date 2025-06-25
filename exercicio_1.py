42c7d229b6e13eabbb58237b46b317352537d657
fesilveiraa <fesilveira23@gmail.com>
2025-06-25 14:09:04 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,12 +1,10 @@
-"""
-Exercício 1 - Escrevendo uma função.
+def verificar_nota (nota):
+    if nota > 5:
+        return ("Aprovado")
+    elif nota < 3:
+        return ("Reprovado")
+    elif 3 > nota < 5:
+        return ("Recuperação")
+    
 
-Escreva uma função com o nome `verificar_nota` que recebe um nota e diz se o aluno foi aprovado, ficou de recuperação ou foi reprovado.
-
-Se a nota foi > 5 -> retorne "Aprovado".
-Se a nota foi entre 3 e 5 -> retorne "Recuperação".
-Se a nota for < 3 -> retorne "Reprovado".
-
-Obs: Escreva necessariamente uma função que retorne (não só imprima) esses valores. De exatamente o nome de `verificar_nota` porque
-o teste vai procurar uma função com esse nome.
-"""
+print (verificar_nota)
