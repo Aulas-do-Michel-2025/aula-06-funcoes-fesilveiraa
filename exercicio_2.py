605680f0dcbbccf8756e046ea79bf668c63d44aa
fesilveiraa <fesilveira23@gmail.com>
2025-07-10 14:50:16 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,10 +1,14 @@
-"""
-Exercício 2 - Criando um dicionário
+def criar_organismo (id, nome, tamanho_do_genoma):
+       return {
+        "id": id,
+        "nome": nome,
+        "tamanho_do_genoma": tamanho_do_genoma
+    }
 
-Escreva uma função com o nome `criar_organismo` que recebe 3 argumentos: id, nome, tamanho_do_genoma e retorna um
-dicionário contendo esses três campos (com as chaves "id", "nome" e "tamanho_do_genoma").
+id = input ("Qual é o id do organismo? ")
+nome = input ("Qual o nome do organismo? ")
+tamanho_do_genoma = input ("Qual o tamanho do genoma? ")
 
-Exemplo de uso:
->>> print(criar_organismo(10, 'HIV', 1000))
->>> {"id": 10, "nome": "HIV", 1000}
-"""
+organismo = criar_organismo (id, nome, tamanho_do_genoma)
+
+print(f"id: {organismo['id']}, nome: {organismo['nome']}, tamanho do genoma: {organismo['tamanho_do_genoma']}")
