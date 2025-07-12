ed82062561f13f5b76a23748c1a3fffc42cac381
fesilveiraa <fesilveira23@gmail.com>
2025-07-12 14:28:07 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -4,11 +4,3 @@ def criar_organismo (id, nome, tamanho_do_genoma):
         "nome": nome,
         "tamanho_do_genoma": tamanho_do_genoma
     }
-
-id = input ("Qual é o id do organismo? ")
-nome = input ("Qual o nome do organismo? ")
-tamanho_do_genoma = input ("Qual o tamanho do genoma? ")
-
-organismo = criar_organismo (id, nome, tamanho_do_genoma)
-
-print(f"id: {organismo['id']}, nome: {organismo['nome']}, tamanho do genoma: {organismo['tamanho_do_genoma']}")
